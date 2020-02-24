3fbf5fc60fd6ed6420398910f5d586bda0361e0f
Fabio Soares Schmidt <fabio@improve.inf.br>
2020-02-24 15:02:35 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+#$DB_DSN="sqlite:////tmp/cluebringer.sqlite";
+#$DB_DSN="mysql:host=localhost;dbname=cluebringer";
+$DB_DSN="sqlite:////opt/zimbra/data/cbpolicyd/db/cbpolicyd.sqlitedb";
+$DB_USER="root";
+#$DB_PASS="";
+#$DB_TABLE_PREFIX="";
